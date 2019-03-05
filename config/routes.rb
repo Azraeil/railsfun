69b632f7ab54844f90087c16c898ef2310648519
Shih_Kuan <cliff1304@gmail.com>
2019-03-05 14:07:50 +0800

    路由設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :statics , :only => [:index]
+  root "statics#index"
+
+  resources :items , :only => [:index, :show]
+  namespace :dashboard do
+    resources :orders
+
+    namespace :asdkjh do
+      resources :items
+      resources :categories
+      resources :orders
+
+      resources :users
+      resources :managers
+    end
+  end
+
 end
